2f8e976a0c174161ca50ad749128dbb8a6eb3388
Shrutimoy Das <shrutimoydas@iitgn.ac.in>
2023-03-30 22:38:35 +0530

    Update q3_test.py

--- q3_test.py
+++ q3_test.py
@@ -22,7 +22,7 @@ y = 3*x + 8 + np.random.normal(0,3,len(x))
 
 y=pd.Series(y)
 LR = LinearRegression(fit_intercept=True)
-LR.fit_gradient_descent_jax(pd.DataFrame(x), y, batch_size=40, num_iters=10, lr=0.01)
+LR.fit_gradient_descent(pd.DataFrame(x), y, batch_size=40, num_iters=10, lr=0.01)
 LR.plot_surface(pd.Series(x),y,LR.all_coef.iloc[0],LR.all_coef.iloc[1])
 LR.plot_line_fit(pd.Series(x),y,LR.all_coef.iloc[0],LR.all_coef.iloc[1])
 LR.plot_contour(pd.Series(x),y,LR.all_coef.iloc[0],LR.all_coef.iloc[1])
